d48b605e33918a8a90103e1e2698ea629d164949
azhuge233 <17763056+azhuge233@users.noreply.github.com>
2024-06-22 11:54:13 +0800

    Remove playwright config entries.

--- UbisoftGiveawayNotifier/Models/Config/Config.cs
+++ UbisoftGiveawayNotifier/Models/Config/Config.cs
@@ -1,7 +1,4 @@
 ﻿namespace UbisoftGiveawayNotifier.Models.Config {
 	public class Config : NotifyConfig {
-		public bool EnableHeadless { get; set; }
-		public bool NotifyKeepGamesOnly { get; set; }
-		public int TimeOutMilliSecond { get; set; }
 	}
 }

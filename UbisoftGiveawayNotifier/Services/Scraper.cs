7f995b4e62f91b57c127ccd3de6c76d5713b919e
azhuge233 <17763056+azhuge233@users.noreply.github.com>
2025-11-14 09:37:43 +0800

    Change free game info source.

--- UbisoftGiveawayNotifier/Services/Scraper.cs
+++ UbisoftGiveawayNotifier/Services/Scraper.cs
@@ -1,23 +1,34 @@
 ﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.Playwright;
+using UbisoftGiveawayNotifier.Models.Config;
 using UbisoftGiveawayNotifier.Strings;
 
 namespace UbisoftGiveawayNotifier.Services {
-	internal class Scraper(ILogger<Scraper> logger) : IDisposable {
-		private readonly ILogger<Scraper> _logger = logger;
+	internal class Scraper : IDisposable {
+		private readonly ILogger<Scraper> _logger;
+		private readonly Config _config;
 
-		internal HttpClient Client { get; set; } = new HttpClient();
+		public Scraper(ILogger<Scraper> logger, IOptions<Config> config) {
+			_logger = logger;
+			_config = config.Value;
+			Microsoft.Playwright.Program.Main(["install", "firefox"]);
+		}
 
 		public async Task<string> GetUbisoftSource() {
-			InitHttpClient();
-
 			try {
 				_logger.LogDebug($"{ScrapeString.debugGetubisoftSource}");
 
-				var response = await Client.GetAsync(ScrapeString.UbisoftPromotionGraphQLRequestUrl);
-				var result = await response.Content.ReadAsStringAsync();
+				var page = await GetNewPageInstance();
+
+				await page.GotoAsync(ScrapeString.UbisoftStoreFreeGamesUrl, new() { WaitUntil = WaitUntilState.Load });
+
+				var source = await page.Locator("body").InnerHTMLAsync();
+
+				await page.CloseAsync();
 
 				_logger.LogDebug($"Done: {ScrapeString.debugGetubisoftSource}");
-				return result;
+				return source;
 			} catch (Exception) {
 				_logger.LogError($"Error: {ScrapeString.debugGetubisoftSource}");
 				throw;
@@ -26,12 +37,53 @@ namespace UbisoftGiveawayNotifier.Services {
 			}
 		}
 
-		private void InitHttpClient() {
-			Client.DefaultRequestHeaders.UserAgent.ParseAdd(ScrapeString.HEADER_VALUE_UA[new Random().Next(0, ScrapeString.HEADER_VALUE_UA.Count - 1)]);
-			// Client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue(ScrapeString.HEADER_VALUE_Authorization);
-			Client.DefaultRequestHeaders.Add(ScrapeString.HEADER_KEY_Authorization, ScrapeString.HEADER_VALUE_Authorization);
-			Client.DefaultRequestHeaders.Add(ScrapeString.HEADER_KEY_AppID, ScrapeString.HEADER_VALUE_AppID);
-			Client.DefaultRequestHeaders.Add(ScrapeString.HEADER_KEY_AppName, ScrapeString.HEADER_VALUE_AppName);
+		private async Task<IPage> GetNewPageInstance() {
+			var playwright = await Playwright.CreateAsync();
+			var browser = await playwright.Firefox.LaunchAsync(new() { Headless = _config.EnableHeadless });
+
+			var context = await browser.NewContextAsync();
+
+			await context.AddCookiesAsync([
+				new Cookie() { // resolve privacy policy popup
+					Name = ScrapeString.bool_UBI_PRIVACY_POLICY_ACCEPTED,
+					Value = ScrapeString.boolCookieValue,
+					Domain = ScrapeString.storeCookieDomain,
+					Path = ScrapeString.cookiePath
+				},
+				new Cookie() { // resolve privacy policy popup
+					Name = ScrapeString.bool_UBI_PRIVACY_POLICY_VIEWED,
+					Value = ScrapeString.boolCookieValue,
+					Domain = ScrapeString.storeCookieDomain,
+					Path = ScrapeString.cookiePath
+				},
+				new Cookie() { // resolve location popup
+					Name = ScrapeString.bool_geopopup_us,
+					Value = ScrapeString.boolCookieValue,
+					Domain = ScrapeString.storeCookieDomain,
+					Path = ScrapeString.cookiePath
+				},
+				new Cookie() {
+					Name = ScrapeString.string_prefCountry,
+					Value = ScrapeString.countryCookieValue_us,
+					Domain = ScrapeString.storeCookieDomain,
+					Path = ScrapeString.cookiePath,
+					SameSite = SameSiteAttribute.None
+				}
+			]);
+
+			var page = await context.NewPageAsync();
+			page.SetDefaultTimeout(_config.TimeOutMilliSecond);
+			page.SetDefaultNavigationTimeout(_config.TimeOutMilliSecond);
+			await page.RouteAsync("**/*", async route => {
+				var blockList = new List<string> { 
+					"image", 
+					"font" 
+				};
+				if (blockList.Contains(route.Request.ResourceType)) await route.AbortAsync();
+				else await route.ContinueAsync();
+			});
+
+			return page;
 		}
 
 		public void Dispose() {
